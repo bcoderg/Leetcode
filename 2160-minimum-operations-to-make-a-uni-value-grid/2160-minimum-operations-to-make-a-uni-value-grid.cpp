ab392813c426a77d02e9b8b6cbc52ac8e0c6d31a
badhrinarayana <123868459+bcoderg@users.noreply.github.com>
2025-03-27 02:00:51 +0530

    Time: 60 ms (12.35%), Space: 95.6 MB (9.34%) - LeetHub

--- 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.cpp
@@ -15,13 +15,22 @@ public:
         int res=INT_MAX;
         sort(nums.begin() , nums.end());
         int n=nums.size();
-        int pre=0;
-        for(int i=0;i<n;i++){
-            int costLeft=nums[i]*i - pre;
-            int costRight=total-pre - (nums[i]*(n-i));
-            int steps=(costLeft+costRight)/x;
-            pre+=nums[i];
-            res=min(res,steps);
+        //A1
+        // int pre=0;
+        // for(int i=0;i<n;i++){
+        //     int costLeft=nums[i]*i - pre;
+        //     int costRight=total-pre - (nums[i]*(n-i));
+        //     int steps=(costLeft+costRight)/x;
+        //     pre+=nums[i];
+        //     res=min(res,steps);
+        // }
+        //return res;
+
+        //A2
+        int median=nums[n/2];
+        res=0;
+        for(int n:nums){
+            res+=(abs(median-n)/x);
         }
         return res;
     }
