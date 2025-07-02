a46e4fcec468e343eca7c24af37f6fbc544c6451
badhrinarayana <123868459+bcoderg@users.noreply.github.com>
2025-07-03 00:20:33 +0530

    Time: 541 ms (34.52%), Space: 272.9 MB (11.9%) - LeetHub

--- 3618-find-the-original-typed-string-ii/3618-find-the-original-typed-string-ii.cpp
+++ 3618-find-the-original-typed-string-ii/3618-find-the-original-typed-string-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<int>>memo;
-    int M=1e9+7; 
+    int M = 1e9 + 7;
     //top down dp - memoization
     /*
     int solve(int i , int count , vector<int>&freq , int k)
@@ -31,64 +31,68 @@ public:
     }
     */
     int possibleStringCount(string word, int k) {
-        int ans=0 , n=word.size();
+        int ans = 0 , n = word.size();
 
-        if(k>n)return 0;
-        
+        if (k > n)return 0;
+
         vector<int>freq;
-        int count=1;
-        for(int i=1;i<n;i++)
+        int count = 1;
+        for (int i = 1; i < n; i++)
         {
-            if(word[i]==word[i-1]){
+            if (word[i] == word[i - 1]) {
                 count++;
             }
-            else{
+            else {
                 freq.push_back(count);
-                count=1;
+                count = 1;
             }
         }
         freq.push_back(count);
 
 
         //mod functions
-        auto modSum=[&](int a , int b)->int{
-            return ( (a%M) + (b%M) )%M;
+        auto modSum = [&](int a , int b)->int{
+            return ( (a % M) + (b % M) ) % M;
         };
-        auto modProduct=[&](int a , int b)->int{
-            return (1LL * (a%M) * (b%M) )%M;
+        auto modProduct = [&](int a , int b)->int{
+            return (1LL * (a % M) * (b % M) ) % M;
+        };
+
+        auto modSub = [&](int a , int b)->int{
+            return ( (a % M) - (b % M) + M ) % M;
         };
 
         int totalPossibilities = 1;
-        for(int &fr:freq){
+        for (int &fr : freq) {
             totalPossibilities = modProduct(totalPossibilities , fr);
         }
-        
-        n=freq.size();
-        if(n >= k)return totalPossibilities;
 
-        memo.assign(n+1 , vector<int>(k+1));
-        
+        n = freq.size();
+        if (n >= k)return totalPossibilities;
+
+        memo.assign(n + 1 , vector<int>(k + 1));
+
         //top down - memoization
-        //int invalidPossibilities = solve(0,0,freq,k); //index , count , freq , 
+        //int invalidPossibilities = solve(0,0,freq,k); //index , count , freq ,
 
 
-        //bottom up dp - tabulation 
+        //bottom up dp - tabulation
         //base case
-        for(int count=k-1;count>=0;count--){
-            memo[n][count]=1;
+        for (int count = k - 1; count >= 0; count--) {
+            memo[n][count] = 1;
         }
 
-        for(int i = n-1 ; i >= 0 ; i --)
+        for (int i = n - 1 ; i >= 0 ; i --)
         {
             //prefix sum
-            vector<int>prefix(k+1, 0);
-            for(int h = 1 ; h <= k ; h++)
+            vector<int>prefix(k + 1, 0);
+            for (int h = 1 ; h <= k ; h++)
             {
-                prefix[h] = ( prefix[h-1] + memo[i+1][h-1] )%M;
+                prefix[h] = modSum(prefix[h - 1] , memo[i + 1][h - 1]);
             }
 
 
-            for(int count = k-1 ; count >= 0 ; count --)
+            for (int count = k - 1 ; count >= 0 ; count --)
             {
                 // int res=0;
                 // for(int take = 1 ; take <= freq[i] ; take ++)
@@ -107,19 +111,18 @@ public:
                 //use prefix sum , avoid this inner loop
                 int l = count + 1 , r = count + freq[i];
 
-                if(r+1 > k){
-                    r=k-1;
+                if (r + 1 > k) {
+                    r = k - 1;
                 }
 
-                if(l<=r){
-                    memo[i][count] = (prefix[r+1] - prefix[l] + M)%M;
+                if (l <= r) {
+                    memo[i][count] = modSub(prefix[r+1] , prefix[l]);
                 }
 
             }
         }
 
         int invalidPossibilities = memo[0][0];
-        
-        return (totalPossibilities - invalidPossibilities + M)%M;
+        return modSub(totalPossibilities , invalidPossibilities);
     }
 };
