3ac7721094e4977a606e05ed452998aa9fa66602
badhrinarayana <123868459+bcoderg@users.noreply.github.com>
2025-07-04 00:21:59 +0530

    Time: 0 ms (100%), Space: 7.8 MB (96.34%) - LeetHub

--- 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
@@ -1,25 +1,10 @@
 class Solution {
 public:
     char kthCharacter(int k) {
-        string s="a";
-
-        auto getGenString = [&](string s)->string{
-            string cur=s , new_str = s;
-            for(int i=0;i<new_str.size() ; i++){
-                if(new_str[i]=='z'){
-                    new_str[i]='a';
-                }
-                else{
-                    new_str[i] = new_str[i] + 1;
-                }
-            }
-            cout<<cur<<" "<<new_str<<"\n";
-            return cur+new_str;
-        };
-
-        while(s.size() < k){
-            s = getGenString(s);
-        }
-        return s[k-1];
+        // we reach to a char only after , doubling from it's /2 position
+        //then increasing the char value by 1 
+        //total increases = total no.of times doubled
+        //=total no.of set bits in binary of k-1 
+        return 'a' + __builtin_popcount(k-1);
     }
 };
