559fe622fe7e5c342a6ecc562660c3df5a415058
badhrinarayana <123868459+bcoderg@users.noreply.github.com>
2025-08-29 22:08:03 +0530

    Time: 1 ms (48.93%), Space: 43.2 MB (10.08%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,23 +1,19 @@
 
 class Solution {
-    void subsets(int[] arr,int i,List<Integer> temp,List<List<Integer>> ans) {
-        //base case
-        if(i==arr.length) {
-            // System.out.println(sum);
-            ans.add(new ArrayList<>(temp));
-            return;
-        }
+    void subsets(int[] arr,int start,List<Integer> temp,List<List<Integer>> ans) {
+        ans.add(new ArrayList<>(temp));
+        for(int i = start ; i<arr.length ; i++)
+        {
         temp.add(arr[i]);
         subsets(arr,i+1,temp,ans); //pick the element
         temp.remove(temp.size()-1);
-        subsets(arr,i+1,temp,ans); //not pick the element
+        }
     }
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> temp = new ArrayList<>();
         subsets(nums,0,temp,ans);
-        return ans;
-        
+        return ans;        
     }
 }
 
