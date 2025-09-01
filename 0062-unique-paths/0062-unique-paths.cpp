f9a7bca7c03d137f447ad5a2c1eca81fbdf4576f
badhrinarayana <123868459+bcoderg@users.noreply.github.com>
2025-09-02 02:19:53 +0530

    Time: 0 ms (100%), Space: 7.7 MB (96.77%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m , vector<int>(n,1));
+        /*vector<vector<int>>dp(m , vector<int>(n,1));
         //robo moves bottom or right 
         //for each cell , pick values from top,left 
         vector<int>pre(n,1);
@@ -12,8 +12,23 @@ public:
                 // cur[j] = (pre[j] + cur[j-1]); //pre[j] is cur[j] in pev step, we can reuse
                 pre[j] += pre[j-1];
             }
-        //     pre = cur;
+        //  pre = cur;
          }
-        return pre[n-1];
+        return pre[n-1];*/
+
+        //better approach than O(m*n) , is using combinatorics
+        //m-1 downs , n-1 rights are needed
+        //represent all possible ways using nCr formula
+        // if(m>n){
+        //     swap(m,n);//to minimise nCr calculation steps
+        // }
+        //nCr = n! / (n-r)! * r! 
+        double res=1;
+        int r = m-1;
+        int N = n + m - 2;
+        for(int i=1;i<=r;i++){
+            res = res * (N - r + i)/i; 
+        }
+        return (int)res;
     }
 };
