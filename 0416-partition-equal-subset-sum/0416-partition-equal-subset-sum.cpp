b8acfb69513c6c88279c3f5cfbee92bd08becb43
badhrinarayana <123868459+bcoderg@users.noreply.github.com>
2025-10-06 15:51:00 +0530

    Time: 60 ms (89.5%), Space: 13.3 MB (98.07%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,36 +1,38 @@
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        int s=0 , n=nums.size();
-        for(auto x:nums)s+=x;
-        if(s&1)return false;
-        s/=2;
-        vector<vector<bool>>dp(n+1  , vector<bool>(s+1 , false));
+        int n = nums.size();
+        int sum = accumulate(nums.begin() , nums.end() , 0);
+        if(sum&1)return false;  
+        sum>>=1;
+        //2D dp
+        // vector<vector<bool>>dp(n+1 , vector<bool>(sum+1 , 0));
+        // for(int i=0;i<=n;i++)dp[i][0]=true; 
 
-        for(int  i=0;i<=n;i++)dp[i][0]=true;
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=s;j++)
-            {
-                if(nums[i-1]<=j)
-                {
-                    dp[i][j]= dp[i-1][j-nums[i-1]] || dp[i-1][j];
-                }
-                else
-                {
-                    dp[i][j] = dp[i-1][j];
+        // for(int i=1;i<=n;i++){
+        //     for(int j=0;j<=sum ; j++){
+        //         if(nums[i-1] <= j){
+        //             dp[i][j] = dp[i-1][j] || dp[i-1][j - nums[i-1]];
+        //         }
+        //         else{
+        //             dp[i][j] = dp[i-1][j];
+        //         }
+        //     }
+        // }
+        // return dp[n][sum];
+
+        //1D DP - can reuse 1 array it self works fine
+        vector<bool>dp(sum+1 , false);
+        dp[0] = true;  
+
+        for(int i=1;i<=n;i++){
+            //for(int j=0;j<=sum ; j++){
+            for(int j=sum; j>=0 ; j--){ //as we are using j - nums[i] , prev values should be computed
+                if(nums[i-1] <= j){
+                    dp[j] = dp[j] || dp[j - nums[i-1]];
                 }
             }
         }
-        // for(int i=0;i<n;i++)
-        // {
-        //     for(int j=0;j<s;j++)
-        //     {
-        //         cout<<dp[i][j]<<" ";
-        //     }
-        //     cout<<"\n";
-        // }
-        return dp[n][s];
-
+        return dp[sum];
     }
 };
